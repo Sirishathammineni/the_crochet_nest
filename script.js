0ce7d7175601e9c4428a0418697ff098d25bcf74
Sirishathammineni <sirishareddyv05@gmail.com>
2025-03-22 21:23:03 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function addToCart(productName, price) {
+document.getElementById("menu-icon").addEventListener("click", function() {
+    document.getElementById("sidebar").style.left = "0";
+});
+document.getElementById("closeSidebar").addEventListener("click", function() {
+    document.getElementById("sidebar").style.left = "-250px";
+});
+
+function addToCart(name, price) {
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
-    cart.push({ name: productName, price: price });
+    cart.push({ name, price });
     localStorage.setItem("cart", JSON.stringify(cart));
-    alert(productName + " added to cart!");
+    alert("Added to cart!");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let user = JSON.parse(localStorage.getItem("user"));
+    if (user) document.getElementById("userDetails").innerText = ${user.username} (${user.email});
+});
